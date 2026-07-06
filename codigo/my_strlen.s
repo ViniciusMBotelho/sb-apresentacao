9d433b3fffb8e4031f2bd35576cb0625c06700b9
Vinicius <vinicius@example.com>
2026-07-06 09:24:41 -0300

    Simplifica o my_strlen

--- codigo/my_strlen.s
+++ codigo/my_strlen.s
@@ -6,7 +6,7 @@
 # Entrada: %rdi = ponteiro para a string (char*)
 # Saída:   %rax = tamanho da string (long long)
 my_strlen:
-    xorq %rax, %rax        # Inicializa o contador de tamanho em 0
+    movq $0, %rax          # Inicializa o contador de tamanho em 0
 .loop:
     cmpb $0, (%rdi,%rax)   # Compara o byte atual com o caractere nulo '\0'
     je .end                # Se for zero (fim da string), pula para o fim
